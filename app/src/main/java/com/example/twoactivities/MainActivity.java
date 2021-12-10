1d0331009937141aa8d2cb5f0ee9ffbf3010589f
shashank gaihre <sharmashashank2055@gmail.com>
2021-12-10 15:50:40 +0545

    finished my homework for this labtest

--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.twoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.twoactivities.extra.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showHello(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        String message = mShowCount.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (this.mShowCount != null)
+            this.mShowCount.setText(Integer.toString(mCount));
     }
 }
